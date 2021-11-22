99ce49f7cf5a1c00a806f2c8420946f882a9bf9d
CY2365 <ckyz1007@gmail.com>
2021-11-22 17:54:59 +0300

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArray, ...remove) {
+    // theArray is going to be modified permanently so newArray is made
+    let newArray = theArray
+    // For every item in the rest parameter remove, check if it occurs
+    // in newArray. If it does, remove that item with splice from
+    // newArray.
+    for (let i in remove) {
+        for (let x in newArray) {
+            if (remove[i] === newArray[x]) {
+                newArray.splice(x, 1)
+            }
+        }
+    }
+    // Return the spliced newArray.
+    return newArray
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
