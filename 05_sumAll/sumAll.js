fc17575d254889fd4de06e7747b54814cf3ea781
CY2365 <ckyz1007@gmail.com>
2021-11-23 21:21:26 +0300

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    // Variable to hold sum of all values. To be returned at the end.
+    var sumOfAll = 0
 
+    // Checks if parameters are a number or not and if it is a positive
+    // value.
+    if (typeof begin !== 'number' || typeof end !== 'number' || 
+        begin < 0 || end < 0) {
+        return 'ERROR'
+    // Checks which value is greater and loops accordingly.
+    } else if (begin < end) {
+        for (let i = begin; i < end +1; i++) {
+            sumOfAll += i
+        }
+    } else if (begin > end) {
+        for (let i = begin; i > end - 1; i--) {
+            sumOfAll += i
+        }
+    }
+    return sumOfAll
 };
 
 // Do not edit below this line
