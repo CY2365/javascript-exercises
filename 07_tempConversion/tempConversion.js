c50fc3932277f42d9306ec85466ceac232404a7c
CY2365 <ckyz1007@gmail.com>
2021-11-24 22:52:30 +0300

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+// Formula for C to F: C * 1.8 + 32
+// Formula for F to C: (F - 32) * (5/9)
 
+const ftoc = function(fahrenheit) {
+    let celsius = (fahrenheit - 32) * (5/9)
+    let rounded = Math.round(celsius * 10) / 10
+    return rounded
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+    let fahrenheit = celsius * 1.8 + 32
+    var rounded = Math.round(fahrenheit * 10) / 10
+    return rounded
 };
 
 // Do not edit below this line
