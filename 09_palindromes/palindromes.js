1816631e1ad6793cd14213803f90617162540a91
CY2365 <ckyz1007@gmail.com>
2021-12-07 20:49:25 +0300

    finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+// used guidance from solutions branch. Needed help with regular
+// expresions. Also used return like in the solution for cleaner code.
 
+const palindromes = function (string) {
+    let cleanString = string.toLowerCase().replace(/[^A-Za-z]/g, "")
+    return (cleanString
+        .split("")
+        .reverse()
+        .join("") == cleanString);
 };
 
 // Do not edit below this line
